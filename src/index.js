d46e1e9bc3a62d42a4a50d9f12f15a1190eba68b
Jake Day <jacobhday@gmail.com>
2020-10-07 20:14:02 -0400

    another commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Greeting() {
-  return <h4>Welcome to the pleasuredome</h4>;
+//CSS
+
+
+// this is a component
+// function Greeting() {
+//   return <h4>Hallo</h4>;
+// }
+function BookList() {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => (
+  <img src='https://images-na.ssl-images-amazon.com/images/I/A1XyS0D1S-L._AC_UL200_SR200,200_.jpg'></img>
+);
+
+const Title = () => <h1>Room on the Broom</h1>;
+
+const Author = () => <h4>Julia Donaldson</h4>;
+
+ReactDom.render(<BookList />, document.getElementById("root"));
